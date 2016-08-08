521a5795b643835de803c5db671ea43c03118bc0
future21star <future21star@yahoo.com>
2016-08-08 08:17:14 -0700

    update

---
+++ app/mailers/invoice_mailer.rb
@@ -0,0 +1,12 @@
+class InvoiceMailer < ApplicationMailer
+
+  def send_pr(sender_email, prs)
+	  #debugger
+	  @sender_email = sender_email
+	  attachments['PRs.xlsx'] = File.read(Rails.root.join('public/prs.xlsx'))
+	  mail(to: 'invoice@bswsuperstores.com', subject: 'Purchase Receipts')
+	  #mail(to: 'owensmith198752@yahoo.com', subject: 'Purchase Receipts')
+	end
+
+end
+
